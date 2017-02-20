f63bd59061712d8a59d2e1f4dd241d7aea87d15f
Beck <13524781846@139.com>
2017-02-20 17:58:49 +0800

    更新polls

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Poll,Choice
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_data')[:5]
+
     return render(request, 'polls/index.html', {'latest_poll_list':latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse('投票问题详情{0}'.format(poll_id))
+    poll = get_object_or_404(Poll, pk=poll_id)
+
+    return render(request, 'polls/detail.html', {'poll':poll})
 
 def results(request, poll_id):
     return HttpResponse('投票问题结果{0}'.format(poll_id))
